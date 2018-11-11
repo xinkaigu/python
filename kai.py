8065429142f213de0a85cedd705b7bc43445424e
guxinkai <13683562007@139.com>
2018-11-11 17:09:53 +0800

    1

---
+++ kai.py
@@ -0,0 +1,9 @@
+while 1 == 1:
+    a = input('开始')
+    if a == '学习':
+        print('8+100=')
+        a = input('输入')
+        if a == '108':
+            print('对了')
+        else:
+            print('错了')
---
+++ study.py
@@ -0,0 +1,10 @@
+a = [1, 2, 3, 2345, 56767, 78765, 87666]
+b = ["mama", "baba", "jiejie", "古", "刘"]
+# print(a[4])
+# print(b)
+# print(len(b))
+    print(i)
+for i in b:
+
+a = '1'
+b = int(a)
