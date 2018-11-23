8c71f774f0b799c0bb8fb603e00ca691cf1b7e9e
guxinkai <13683562007@139.com>
2018-11-23 18:58:51 +0800

    两位数计算器

--- kai.py
+++ kai.py
@@ -1,21 +1,18 @@
-import random
-
-print('开始练习')
-h = 0
-j = 0
-while h < 51:
-    s = (random.randint(0, 10))
-    d = (random.randint(0, 10))
-    print('计算' + str(s) + '+' + str(d))
-    f = s + d
-    g = input()
-    if int(g) == f:
-        print('对了')
-        h = h + 1
-        j = j + 1
-        print('您已做' + str(j) + '道题')
+while 1 == 1:
+    a = input()
+    d = input()
+    s = input()
+    if d == '*':
+        f = int(a) * int(s)
+        print(int(f))
+    elif d == '/':
+        f = int(a) / int(s)
+        print(int(f))
+    elif d == '+':
+        f = int(a) + int(s)
+        print(int(f))
+    elif d == '-':
+        f = int(a) - int(s)
+        print(int(f))
     else:
-        print('错了')
-        h = 0
-        j = 0
-        print('您已做' + str(j) + '道题')
+        print('程序不对')
