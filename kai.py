24688ab1089afd316eb1c0c7bce227a70b098a6c
guxinkai <13683562007@139.com>
2018-11-24 09:42:54 +0800

    练习

--- kai.py
+++ kai.py
@@ -1,18 +1,63 @@
+# while 1 == 1:
+#     a = input()
+#     d = input()
+#     s = input()
+#     if d == '*':
+#         f = int(a) * int(s)
+#         print(int(f))
+#     elif d == '/':
+#         f = int(a) / int(s)
+#         print(int(f))
+#     elif d == '+':
+#         f = int(a) + int(s)
+#         print(int(f))
+#     elif d == '-':
+#         f = int(a) - int(s)
+#         print(int(f))
+#     else:
+#         print('程序不对')
+
 while 1 == 1:
-    a = input()
-    d = input()
-    s = input()
-    if d == '*':
-        f = int(a) * int(s)
-        print(int(f))
-    elif d == '/':
-        f = int(a) / int(s)
-        print(int(f))
-    elif d == '+':
-        f = int(a) + int(s)
-        print(int(f))
-    elif d == '-':
-        f = int(a) - int(s)
-        print(int(f))
+    A = input('输入数字')
+    B = input('输入符号')
+    C = input('输入数字')
+    D = input('输入等于或符号')
+    if D == '=':
+        if B == '*':
+            Q = int(A) * int(C)
+            input(Q)
+        elif B == '/':
+            Q = int(A) / int(C)
+            print(Q)
+        elif B == '+':
+            Q = int(A) + int(C)
+            print(Q)
+        elif B == '-':
+            Q = int(A) - int(C)
+            print(Q)
+        else:
+            print('程序错误')
     else:
-        print('程序不对')
+        E = input('输入数字')
+        if B == '*':
+            Q = int(A) * int(C)
+        elif B == '/':
+            Q = int(A) / int(C)
+        elif B == '+':
+            Q = int(A) + int(C)
+        elif B == '-':
+            Q = int(A) - int(C)
+        if D == '*':
+            W = int(Q) * int(E)
+            input(W)
+        elif D == '/':
+            W = int(Q) / int(E)
+            print(W)
+        elif D == '+':
+            W = int(Q) + int(E)
+            print(W)
+        elif D == '-':
+            W = int(Q) - int(E)
+            print(W)
+        else:
+            print('程序错误')
