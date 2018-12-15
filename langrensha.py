c00c7e238037232adad5125a9ceb865adde8a85c
guxinkai <13683562007@139.com>
2018-12-15 11:12:33 +0800

    狼人杀

---
+++ langrensha.py
@@ -0,0 +1,562 @@
+import random
+
+v = 0
+while 1 == 1:
+    a = (random.randint(1, 7))
+    if a == 1:
+        a = (random.randint(1, 7))
+    elif a == 2:
+        a = (random.randint(1, 7))
+    elif a == 3:
+        a = (random.randint(1, 7))
+    b = (random.randint(1, 7))
+    if b == 1:
+        b = (random.randint(1, 7))
+    elif b == 2:
+        b = (random.randint(1, 7))
+    elif b == 3:
+        b = (random.randint(1, 7))
+    c = (random.randint(1, 7))
+    if c == 1:
+        c = (random.randint(1, 7))
+    elif c == 2:
+        c = (random.randint(1, 7))
+    elif c == 3:
+        c = (random.randint(1, 7))
+    d = (random.randint(1, 7))
+    if d == 1:
+        d = (random.randint(1, 7))
+    elif d == 2:
+        d = (random.randint(1, 7))
+    elif d == 3:
+        d = (random.randint(1, 7))
+    e = (random.randint(1, 7))
+    if e == 1:
+        e = (random.randint(1, 7))
+    elif e == 2:
+        e = (random.randint(1, 7))
+    elif e == 3:
+        e = (random.randint(1, 7))
+    f = (random.randint(1, 7))
+    if f == 1:
+        f = (random.randint(1, 7))
+    elif f == 2:
+        f = (random.randint(1, 7))
+    elif f == 3:
+        f = (random.randint(1, 7))
+    g = (random.randint(1, 7))
+    if g == 1:
+        g = (random.randint(1, 7))
+    elif g == 2:
+        g = (random.randint(1, 7))
+    elif g == 3:
+        g = (random.randint(1, 7))
+    if a == 1:
+        print('首领')
+        input()
+        qw = 1
+        zxc = '首领'
+
+    elif a == 2:
+        print('女巫')
+        input()
+        qw = 2
+        zxc = '女巫'
+
+    elif a == 3:
+        print('预言家')
+        input()
+        qw = 1
+        zxc = '预言家'
+
+    elif a == 4:
+        print('狼人')
+        input()
+        qw = 1
+        zxc = '狼人'
+
+    elif a == 5:
+        print('平民')
+        input()
+        qw = 1
+        zxc = '平民'
+
+    elif a == 6:
+        print('猎人')
+        input()
+        qw = 1
+        zxc = '猎人'
+
+    else:
+        print('白痴')
+        input()
+        qw = 100
+        zxc = '白痴'
+
+    if b == 1:
+        print('首领')
+        input()
+        we = 1
+        xcv = '首领'
+
+    elif b == 2:
+        print('女巫')
+        input()
+        we = 2
+        xcv = '女巫'
+
+    elif b == 3:
+        print('预言家')
+        input()
+        we = 1
+        xcv = '预言家'
+
+    elif b == 4:
+        print('狼人')
+        input()
+        we = 1
+        xcv = '狼人'
+
+    elif b == 5:
+        print('平民')
+        input()
+        we = 1
+        xcv = '平民'
+
+    elif b == 6:
+        print('猎人')
+        input()
+        we = 1
+        xcv = '猎人'
+
+    else:
+        print('白痴')
+        input()
+        we = 100
+        xcv = '白痴'
+
+    if c == 1:
+        print('首领')
+        input()
+        er = 1
+        cvb = '首领'
+
+    elif c == 2:
+        print('女巫')
+        input()
+        er = 2
+        cvb = '女巫'
+
+    elif c == 3:
+        print('预言家')
+        input()
+        er = 1
+        cvb = '预言家'
+
+    elif c == 4:
+        print('狼人')
+        input()
+        er = 1
+        cvb = '狼人'
+
+    elif c == 5:
+        print('平民')
+        input()
+        er = 1
+        cvb = '平民'
+
+    elif c == 6:
+        print('猎人')
+        input()
+        er = 1
+        cvb = '猎人'
+
+    else:
+        print('白痴')
+        input()
+        er = 100
+        cvb = '白痴'
+
+    if d == 1:
+        print('首领')
+        input()
+        rt = 2
+        vbn = '首领'
+
+    elif d == 2:
+        print('女巫')
+        input()
+        rt = 1
+        vbn = '女巫'
+
+    elif d == 3:
+        print('预言家')
+        input()
+        rt = 1
+        vbn = '预言家'
+
+    elif d == 4:
+        print('狼人')
+        input()
+        rt = 1
+        vbn = '狼人'
+
+    elif d == 5:
+        print('平民')
+        input()
+        rt = 1
+        vbn = '平民'
+
+    elif d == 6:
+        print('猎人')
+        input()
+        rt = 1
+        vbn = '猎人'
+
+    else:
+        print('白痴')
+        input()
+        rt = 100
+        vbn = '白痴'
+
+    if e == 1:
+        print('首领')
+        input()
+        ty = 1
+        bnm = '首领'
+    elif e == 2:
+        print('女巫')
+        input()
+        ty = 2
+        bnm = '女巫'
+
+    elif e == 3:
+        print('预言家')
+        input()
+        ty = 1
+        bnm = '预言家'
+
+    elif e == 4:
+        print('狼人')
+        input()
+        ty = 1
+        bnm = '狼人'
+
+    elif e == 5:
+        print('平民')
+        input()
+        ty = 1
+        bnm = '平民'
+
+    elif e == 6:
+        print('猎人')
+        input()
+        ty = 1
+        bnm = '猎人'
+
+    else:
+        print('白痴')
+        input()
+        ty = 100
+        bnm = '白痴'
+
+    if f == 1:
+        print('首领')
+        input()
+        yu = 1
+        nml = '首领'
+
+    elif f == 2:
+        print('女巫')
+        input()
+        yu = 2
+    elif f == 3:
+        nml = '女巫'
+
+        print('预言家')
+        input()
+        yu = 1
+        nml = '预言家'
+
+    elif f == 4:
+        print('狼人')
+        input()
+        yu = 1
+        nml = '狼人'
+
+    elif f == 5:
+        print('平民')
+        input()
+        yu = 1
+        nml = '平民'
+
+    elif f == 6:
+        print('猎人')
+        input()
+        yu = 1
+        nml = '猎人'
+
+    else:
+        print('白痴')
+        input()
+        yu = 100
+        nml = '白痴'
+
+    if g == 1:
+        print('首领')
+        input()
+        ui = 1
+        lkj = '首领'
+
+    elif g == 2:
+        print('女巫')
+        input()
+        ui = 2
+        lkj = '女巫'
+
+    elif g == 3:
+        print('预言家')
+        input()
+        ui = 1
+        lkj = '预言家'
+
+    elif g == 4:
+        print('狼人')
+        input()
+        ui = 1
+        lkj = '狼人'
+
+    elif g == 5:
+        print('平民')
+        input()
+        ui = 1
+        lkj = '平民'
+
+    elif g == 6:
+        print('猎人')
+        input()
+        ui = 1
+        lkj = '猎人'
+
+    else:
+        print('白痴')
+        input()
+        ui = 100
+        lkj = '白痴'
+    while v == 0:
+        print('天黑请闭眼')
+        input()
+        print('狼人请睁眼')
+        input()
+        print('请选择你们要杀的人')
+        input()
+        o = input('选吧')
+        if o == '1':
+            qw = qw - 1
+        elif o == '2':
+            we = we - 1
+        elif o == '3':
+            er = er - 1
+        elif o == '4':
+            rt = rt - 1
+        elif o == '5':
+            ty = ty - 1
+        elif o == '6':
+            yu = yu - 1
+        elif o == '7':
+            ui = ui - 1
+
+        print('狼人请闭眼')
+        input()
+        print('女巫请睁眼')
+        input()
+        print('今晚被杀的人是' + str(o) + '号')
+        input()
+        print('你要使用毒药或解药')
+        input()
+        qwe = input('毒药或解药')
+        input()
+        if qwe == '解药':
+            if wer == '1':
+                qw = qw + 1
+            elif wer == '2':
+                we = we + 1
+            elif wer == '3':
+                er = er + 1
+            elif wer == '4':
+                rt = rt + 1
+            elif wer == '5':
+                ty = ty + 1
+            elif wer == '6':
+                yu = yu + 1
+            elif wer == '7':
+                ui = ui + 1
+        else:
+            input()
+            wer = input('你要杀谁')
+            if wer == '1':
+                qw = qw - 1
+            elif wer == '2':
+                we = we - 1
+            elif wer == '3':
+                er = er - 1
+            elif wer == '4':
+                rt = rt - 1
+            elif wer == '5':
+                ty = ty - 1
+            elif wer == '6':
+                yu = yu - 1
+            elif wer == '7':
+                ui = ui - 1
+        print('女巫请闭眼')
+        input()
+        print('预言家请睁眼')
+        input()
+        print('你要预言谁')
+        input()
+        p = input('选吧')
+        if p == '1':
+            print(zxc)
+        elif p == '2':
+            print(xcv)
+        elif p == '3':
+            print(cvb)
+        elif p == '4':
+            print(cvb)
+        elif p == '5':
+            print(vbn)
+        elif p == '6':
+            print(nml)
+        elif p == '7':
+            print(lkj)
+        input()
+        print('预言家请闭眼')
+        input()
+        print('首领请睁眼')
+        input()
+        print('请选择你要杀的人')
+        input()
+        k = input('选吧')
+        if k == '1':
+            qw = qw - 1
+        elif k == '2':
+            we = we - 1
+        elif k == '3':
+            er = er - 1
+        elif k == '4':
+            rt = rt - 1
+        elif k == '5':
+            ty = ty - 1
+        elif k == '6':
+            yu = yu - 1
+        elif k == '7':
+            ui = ui - 1
+        print('首领请闭眼')
+        input()
+        print('天亮了')
+        input()
+        if qw == 0:
+            print('1号被杀')
+            print('身份是' + zxc)
+        if we == 0:
+            print('2号被杀')
+            print('身份是' + xcv)
+        if er == 0:
+            print('3号被杀')
+            print('身份是' + cvb)
+        if rt == 0:
+            print('4号被杀')
+            print('身份是' + vbn)
+        if ty == 0:
+            print('5号被杀')
+            print('身份是' + bnm)
+        if yu == 0:
+            print('6号被杀')
+            print('身份是' + nml)
+        if ui == 0:
+            print('7号被杀')
+            print('身份是' + lkj)
+
+        if qw == 0:
+            print('1号被杀')
+            print('身份是' + zxc)
+        elif we == 0:
+            print('2号被杀')
+            print('身份是' + xcv)
+        elif er == 0:
+            print('3号被杀')
+            print('身份是' + cvb)
+        elif rt == 0:
+            print('4号被杀')
+            print('身份是' + vbn)
+        elif ty == 0:
+            print('5号被杀')
+            print('身份是' + bnm)
+        elif yu == 0:
+            print('6号被杀')
+            print('身份是' + nml)
+        elif ui == 0:
+            print('7号被杀')
+            print('身份是' + lkj)
+        else:
+            print('今晚是个平安夜')
+        input()
+        print('投票')
+        input()
+        qaz = input('同意投1号的人请举手')
+        wsx = input('同意投2号的人请举手')
+        edc = input('同意投3号的人请举手')
+        rfv = input('同意投4号的人请举手')
+        tgb = input('同意投5号的人请举手')
+        yhn = input('同意投6号的人请举手')
+        ujm = input('同意投7号的人请举手')
+        plm = max(qaz, wsx, edc, rfv, tgb, yhn, ujm)
+        if plm == qaz:
+            qw = qw - 100000000000000000000000000000
+            print('1号已处死')
+            input()
+            print('公布身份')
+            input()
+            print(zxc)
+        elif wsx == plm:
+            we = we - 100000000000000000000000
+            print('2号已处死')
+            input()
+            print('公布身份')
+            input()
+            print(xcv)
+        elif edc == plm:
+            er = er - 10000000000000000000000000
+            print('3号已处死')
+            input()
+            print('公布身份')
+            input()
+            print(cvb)
+        elif rfv == plm:
+            rt = rt - 1000000000000000000000000
+            print('4号已处死')
+            input()
+            print('公布身份')
+            input()
+            print(vbn)
+        elif tgb == plm:
+            ty = ty - 10000000000000000000000000
+            print('5号已处死')
+            input()
+            print('公布身份')
+            input()
+            print(bnm)
+        elif yhn == plm:
+            yu = yu - 1000000000000000000000
+            print('6号已处死')
+            input()
+            print('公布身份')
+            input()
+            print(nml)
+        elif ujm == plm:
+            ui = ui - 10000000000000000000000
+            print('6号已处死')
+            input()
+            print('公布身份')
+            input()
+            print(lkj)
